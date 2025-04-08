d2f89a1306b774c09065f92c9f7e745cd64bc400
BadrRibzat <badrribzat@gmail.com>
2025-04-08 18:28:15 +0100

    Fixed broken api_auth_login in urls.py and enabled debug temporarily

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,16 +3,6 @@ URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -20,15 +10,6 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view, permission_classes
-
-@csrf_exempt
-@api_view(['GET', 'POST'])
-@permission_classes([permissions.AllowAny])
-def api_auth_login(request):
-    return include('rest_framework.urls')(request)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -54,8 +35,7 @@ def redoc_redirect(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chatbot.urls')),
-    path('api-auth/login/', api_auth_login, name='api-auth-login'),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),  # Standard DRF auth URLs
     path('', swagger_redirect, name='schema-swagger-ui'),
     path('redoc/', redoc_redirect, name='schema-redoc'),
 ]
